a5f95887c6d2cbc741de0c6b6fe6d2f5b6c156fd
Deepesh Mohnani <mohnani@gmail.com>
2012-06-16 02:35:27 -0700

    Add SkyNotify Roaming sample

---
+++ Samples/WindowsReleasePreview/XAML/SkyNotifyRoamSample/WNSSkyDriveService-ServerSide/WNSSkyDriveService/ISkyDriveService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WNSSkyDriveService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ISkyDriveService" in both code and config file together.
+    [ServiceContract]
+    public interface ISkyDriveService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "UpdateChannel?HostName={HostName}&LiveId={userLiveId}&ChannelUri={ChannelUri}&ChannelExpirationTime={ChannelExpirationTime}", RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json)]
+        string UpdateChannelApp(string HostName, string userLiveId, string ChannelUri, string ChannelExpirationTime);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "SendNotify?IDFrom={userLiveIDFrom}&IDTo={userLiveIDTo}&Message1={Message1}&Message2={Message2}&PicURL={PictureURL}", RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json)]
+        string sendNotify(string userLiveIDFrom, string userLiveIDTo, string Message1, string Message2, string PictureURL);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Add?x={Number1}&y={Number2}", RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json)]
+        int Add(string Number1, string Number2);
+    }
+}
